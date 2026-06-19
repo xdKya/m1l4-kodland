525795a664197dbc6d75cc467639b6a6897fc114
xdKya <90745676+xdKya@users.noreply.github.com>
2026-06-19 17:34:19 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Estamos logados como {bot.user}')
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Eaí, Firmeza??')
+
+
+@bot.command()
+async def heh(ctx, count_heh=5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def genpass(ctx):
+    await ctx.send(gen_pass(10))
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+bot.run(
+    "")
